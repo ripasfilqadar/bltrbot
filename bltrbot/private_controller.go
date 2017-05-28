15c614878faafab092c6fed871c1fc0811d2d4e0
ripas.filqadar <ripas.filqadar@gmail.com>
2017-05-28 20:03:12 +0700

    add broadcast message for admin

--- bltrbot/private_controller.go
+++ bltrbot/private_controller.go
@@ -1,9 +1,28 @@
 package main
 
+import (
+  "fmt"
+  "github.com/ripasfilqadar/bltrbot/bltrbot/db"
+  "github.com/ripasfilqadar/bltrbot/bltrbot/model"
+)
+
 func (c *Controller) RecapitulationToday() {
   updateRemaining()
 }
 
 func (c *Controller) ReminderUser() {
   reminderUser()
+}
+
+func (c *Controller) BroadcastMessage() {
+  groups := []model.Group{}
+  db.MysqlDB().Find(&groups)
+  text := ""
+  for i := 1; i < len(Args); i++ {
+    text += Args[i] + " "
+  }
+  fmt.Println(text)
+  for _, group := range groups {
+    Bot.SendToGroup(group.GroupId, text)
+  }
 }
