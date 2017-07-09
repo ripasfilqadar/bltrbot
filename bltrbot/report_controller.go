4a96a8038e8cb3a6f38b3750cb7432f84e4981c2
ripas.filqadar <ripas.filqadar@gmail.com>
2017-07-09 12:32:08 +0700

    rrrrr

--- bltrbot/report_controller.go
+++ bltrbot/report_controller.go
@@ -27,7 +27,6 @@ func (c *Controller) ListToday() {
 }
 
 func (c *Controller) ListIqob() {
-	panic("something wrong babes")
 	var month int
 	var t_min, t_max time.Time
 	var pt_t_min, pt_t_max *time.Time
